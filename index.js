d9608cc7b58f20f743e8ccccedeb1d224bde8f0c
Zeeshan Syed <zsyed89@gmail.com>
2018-11-07 21:08:38 -0500

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+    Primary file for the API
+*/
+
+// Dependencies
+
+// Declare the app
+var app = {};
+
+// Initialize function
+app.init = function() {
+    // Start the server
+};
+
+// Execute the app
+app.init();
+
+// Export the app
+module.exports = app;
